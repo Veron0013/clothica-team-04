d218b80b3c8b68c7b07eb56bbc29d8767de86ebf
Irina <Irysichka2704@gmail.com>
2025-11-09 18:31:01 +0200

    header

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -1,54 +1,71 @@
 "use client";
 
-import { useState } from "react";
-import Link from "next/link";
+import { useState, useEffect } from "react";
 import css from "./Header.module.css";
+import BurgerMenu from "../BurgerMenu/BurgerMenu";
 
 export default function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
 
+    useEffect(() => {
+    if (menuOpen) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "";
+    }
+  }, [menuOpen]);
+
   return (
     <div className={css.container}>
       <header className={css.header}>
-        <Link href="/" className={css.logo}>
+        <a href="" className={css.logo}>
           <svg width="84" height="36" aria-label="Clothica logo">
             <use href="/sprite.svg#icon-company-logo" />
           </svg>
-        </Link>
-        <nav className={css.nav}>
-          <Link href="/">Головна</Link>
-          <Link href="/products">Товари</Link>
-          <Link href="/categories">Категорії</Link>
-        </nav>
-
+        </a>
+        <ul className={css.nav}>
+          <li>
+            <a href="">Головна</a>
+          </li>
+          <li>
+            <a href="">Товари</a>
+          </li>
+          <li>
+            <a href="">Категорії</a>
+          </li>
+        </ul>
         <div className={css.auth}>
-          <Link href="/login" className={css.navUp}>
+          <a href="" className={css.navUp}>
             Вхід
-          </Link>
-          <Link href="/register" className={css.navUp}>
+          </a>
+          <a href="" className={css.navIn}>
             Реєстрація
-          </Link>
-           <Link href="/basket" className={css.navUpBasket}>
-            Кошик
-          </Link>
-        <div className={css.navCont}>
-          <button
-            className={css.burger}
-            onClick={() => setMenuOpen(!menuOpen)}
-            aria-label="Відкрити меню"
-          >
-            <svg width="24" height="24">
-              <use href={`/sprite.svg#${menuOpen ? "close" : "menu"}`} />
-            </svg>
-          </button>
-          <div className={css.basket}>
-            <svg width="24" height="24">
-              <use href="/sprite.svg#shopping_cart" />
-            </svg>
+          </a>
+          {/* <a href="" className={css.navUpBasket}>
+            Кабінет
+          </a> */}
+          <div className={css.navCont}>
+            <button
+              className={css.burger}
+              onClick={() => setMenuOpen(!menuOpen)}
+              aria-label="Відкрити меню"
+            >
+              <svg width="24" height="24">
+                <use href={`/sprite.svg#${menuOpen ? "close" : "menu"}`} />
+              </svg>
+            </button>
+            <div className={css.basket}>
+              <svg width="24" height="24">
+                <use href="/sprite.svg#shopping_cart" />
+              </svg>
+            </div>
           </div>
-          </div>
-          </div>
+        </div>
       </header>
+
+      {menuOpen && (
+          <BurgerMenu menuOpen={menuOpen}/>
+      )}
     </div>
   );
 }
