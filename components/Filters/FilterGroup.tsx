ae86138a83c00d59ca8a9f25dc3bc76454587297
Tetiana <Korostijtatiana@gmail.com>
2025-11-15 19:27:39 +0200

    add

--- components/Filters/FilterGroup.tsx
+++ components/Filters/FilterGroup.tsx
@@ -1,30 +1,36 @@
 // components/Filters/FilterGroup.tsx
-"use client"
+"use client";
 
-import FilterItem from "./FilterItem"
-import css from "./Filter.module.css"
+import FilterItem from "./FilterItem";
+import css from "./FilterGroup.module.css";
 
 export default function FilterGroup({
-	title,
-	name,
-	options,
-	onClose,
+  title,
+  name,
+  options,
+  onClose,
 }: {
-	title: string
-	name: string
-	options: { value: string; label: string }[]
-	onClose?: () => void
+  title: string;
+  name: string;
+  options: { value: string; label: string }[];
+  onClose?: () => void;
 }) {
-	if (!options?.length) return null
+  if (!options?.length) return null;
 
-	return (
-		<section className={css.filter__group} aria-label={title}>
-			<h4 className={css.filter__title}>{title}</h4>
-			<ul className={css.filter__list}>
-				{options.map((opt) => (
-					<FilterItem key={opt.value} name={name} value={opt.value} label={opt.label} onClose={onClose} />
-				))}
-			</ul>
-		</section>
-	)
+  return (
+    <section className={css.filterGroup} aria-label={title}>
+      <h4 className={css.filterTitle}>{title}</h4>
+      <ul className={css.filterList}>
+        {options.map((opt) => (
+          <FilterItem
+            key={opt.value}
+            name={name}
+            value={opt.value}
+            label={opt.label}
+            onClose={onClose}
+          />
+        ))}
+      </ul>
+    </section>
+  );
 }
