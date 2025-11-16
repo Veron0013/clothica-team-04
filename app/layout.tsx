0ce2e9ea9d14cfe686a96db5422f1840448d2a95
Irina <Irysichka2704@gmail.com>
2025-11-16 16:37:56 +0200

    profile styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Toaster } from "react-hot-toast";
 import AuthProvider from "@/components/AuthProvider/AuthProvider";
 import AuthPagesLayout from "@/components/AuthPagesLayout/AuthPagesLayout";
+import ProfilePage from "./(private routes)/profile/page";
 
 const interSans = Inter({
   variable: "--font-inter",
@@ -57,6 +58,7 @@ export default function RootLayout({
         <TanStackProvider>
           <div className="layout">
             <AuthProvider>
+              <ProfilePage />
               <Suspense fallback={<Loading />}>
                 <AuthPagesLayout>{children}</AuthPagesLayout>
               </Suspense>
