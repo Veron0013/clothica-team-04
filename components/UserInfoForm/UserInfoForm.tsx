3cce92b1dba87d5870a7c9a3d364869c070114b1
Mr.Igor <iv_mirsoft@ukr.net>
2025-11-18 13:05:17 +0200

    many form fixes

--- components/UserInfoForm/UserInfoForm.tsx
+++ components/UserInfoForm/UserInfoForm.tsx
@@ -1,129 +1,192 @@
-"use client"
+'use client';
 
-import { ErrorMessage, Field, Form, Formik, FormikHelpers } from "formik"
-import * as Yup from "yup"
-import css from "./UserInfoForm.module.css"
-import { useRouter } from "next/navigation"
-import { useMutation } from "@tanstack/react-query"
-import { updateMe } from "@/lib/api/clientApi"
-import { PHONE_REGEXP } from "@/lib/vars"
-import { useAuthStore } from "@/stores/authStore"
-import toastMessage, { MyToastType } from "@/lib/messageService"
+import { ErrorMessage, Field, Form, Formik, FormikHelpers } from 'formik';
+import * as Yup from 'yup';
+import css from './UserInfoForm.module.css';
+import { useRouter } from 'next/navigation';
+import { useMutation } from '@tanstack/react-query';
+import { updateMe } from '@/lib/api/clientApi';
+import { PHONE_REGEXP } from '@/lib/vars';
+import { useAuthStore } from '@/stores/authStore';
+import toastMessage, {
+  MyToastType,
+  normalizePhone,
+} from '@/lib/messageService';
 
 const UserInfoFormSchema = Yup.object().shape({
-	name: Yup.string().min(2).max(20).required("Це поле обовʼязкове!"),
-	lastname: Yup.string().min(3).max(20).required("Це поле обовʼязкове!"),
-	phone: Yup.string().matches(PHONE_REGEXP, "Введіть коректний номер телефону").required("Це поле обовʼязкове!"),
-	city: Yup.string().min(3).required("Це поле обовʼязкове!"),
-	warehoseNumber: Yup.number().min(1).max(10).required("Це поле обовʼязкове!"),
-})
+  name: Yup.string().min(2).max(20).required('Це поле обовʼязкове!'),
+  lastname: Yup.string().min(3).max(20).required('Це поле обовʼязкове!'),
+  phone: Yup.string()
+    .matches(PHONE_REGEXP, 'Введіть коректний номер телефону')
+    .required('Це поле обовʼязкове!'),
+  city: Yup.string().min(3).required('Це поле обовʼязкове!'),
+  warehoseNumber: Yup.number()
+    .typeError('Вкажіть номер відділення цифрами')
+    .min(1, 'Номер не може бути меншим за 1')
+    .required('Це поле обовʼязкове!'),
+});
 
 interface UserInfoFormValues {
-	name: string
-	lastname: string
-	phone: string
-	city: string
-	comment?: string
-	warehoseNumber: number
+  name: string;
+  lastname: string;
+  phone: string;
+  city: string;
+  comment?: string;
+  warehoseNumber: string;
 }
 
-interface Props {
-	isOrder: boolean
-}
+export default function UserInfoForm() {
+  const user = useAuthStore(state => state.user);
+  const setUser = useAuthStore(state => state.setUser);
 
-export default function UserInfoForm({ isOrder = false }: Props) {
-	const user = useAuthStore((state) => state.user)
+  //console.log("form-user", user, user?.name)
 
-	//console.log("form-user", user, user?.name)
+  const initialValues: UserInfoFormValues = {
+    name: user?.name || '',
+    lastname: user?.lastname || user?.lastname || '',
+    phone: user?.phone || '',
+    city: user?.city || '',
+    warehoseNumber: user?.warehoseNumber || user?.warehoseNumber || '',
+  };
+  const router = useRouter();
 
-	const initialValues: UserInfoFormValues = {
-		name: "Ваше імʼя",
-		lastname: "Ваше прізвище",
-		phone: user?.phone ? user.phone : "+38(0__) ___- __ - __",
-		city: "Ваше місто",
-		warehoseNumber: 1,
-		comment: "Введіть ваш коментар",
-	}
+  const mutation = useMutation({
+    mutationFn: (data: UserInfoFormValues) => updateMe(data),
+    onSuccess: updatedUser => {
+      toastMessage(MyToastType.success, 'Ви успішно відредагували дані!');
+      router.push('/');
+      setUser(updatedUser);
+    },
+  });
 
-	const router = useRouter()
+  const handleSubmit = (
+    values: UserInfoFormValues,
+    actions: FormikHelpers<UserInfoFormValues>
+  ) => {
+    const userValues = { ...values };
+    userValues.phone = normalizePhone(userValues.phone) || userValues.phone;
 
-	const mutation = useMutation({
-		mutationFn: (data: UserInfoFormValues) => updateMe(data),
-		onSuccess: () => {
-			toastMessage(MyToastType.success, "Ви успішно відредагували дані!")
-			router.push("/")
-		},
-	})
+    mutation.mutate(userValues, {
+      onSettled: () => {
+        actions.setSubmitting(false);
+      },
+      onSuccess: () => {
+        actions.resetForm({ values });
+      },
+    });
+  };
 
-	const handleSubmit = (values: UserInfoFormValues, actions: FormikHelpers<UserInfoFormValues>) => {
-		mutation.mutate(values, {
-			onSettled: () => {
-				actions.setSubmitting(false)
-			},
-			onSuccess: () => {
-				actions.resetForm({ values }) // оставляем актуальные значения
-			},
-		})
-	}
-	return (
-		<div className={css.order_container}>
-			<Formik initialValues={initialValues} validationSchema={UserInfoFormSchema} onSubmit={handleSubmit}>
-				{({ isSubmitting }) => (
-					<Form className={css.form}>
-						<fieldset className={css.form}>
-							<legend className={css.text}>Особиста інформація</legend>
-							<div className={css.label_wrapper}>
-								<div className={css.label}>
-									<label htmlFor="name">Імʼя*</label>
-									<ErrorMessage name="name" component="p" className={css.error} />
-									<Field id="name" className={css.input} type="text" name="name" />
-								</div>
-								<div className={css.label}>
-									<label htmlFor="lastname">Прізвище*</label>
-									<Field id="lastname" className={css.input} type="text" name="lastname" />
-									<ErrorMessage name="lastname" component="p" className={css.error} />
-								</div>
-							</div>
-							<div className={css.label}>
-								<label htmlFor="phone">Номер телефону*</label>
-								<Field id="phone" className={css.inputTel} type="tel" name="phone" />
-								<ErrorMessage name="phone" component="p" className={css.error} />
-							</div>
-							<div className={css.label_wrapper}>
-								<div className={css.label}>
-									<label htmlFor="city">Місто доставки*</label>
-									<Field id="city" className={css.input} type="text" name="city" />
-									<ErrorMessage name="city" component="p" className={css.error} />
-								</div>
-								<div className={css.label}>
-									<label htmlFor="warehoseNumber">Номер відділення Нової Пошти*</label>
+  const getInputClass = (error: unknown, touched: boolean | undefined) => {
+    return error && touched ? `${css.input} ${css.inputError}` : css.input;
+  };
 
-									<Field id="warehoseNumber" className={css.input} type="number" name="warehoseNumber" />
-									<ErrorMessage name="warehoseNumber" component="p" className={css.error} />
-								</div>
-							</div>
-							{isOrder && (
-								<div className={css.label}>
-									<label htmlFor="comment">Коментар</label>
+  return (
+    <div className={css.order_container}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={UserInfoFormSchema}
+        onSubmit={handleSubmit}
+      >
+        {({ isSubmitting, errors, touched }) => (
+          <Form className={css.form}>
+            <fieldset className={css.form}>
+              <legend className={css.text}>Особиста інформація</legend>
+              <div className={css.label_wrapper}>
+                <div className={css.label}>
+                  <label htmlFor="name">Імʼя*</label>
+                  <ErrorMessage
+                    name="name"
+                    component="p"
+                    className={css.error}
+                  />
+                  <Field
+                    id="name"
+                    className={getInputClass(errors.name, touched.name)}
+                    type="text"
+                    name="name"
+                    placeholder="Ваше імʼя"
+                  />
+                </div>
+                <div className={css.label}>
+                  <label htmlFor="lastname">Прізвище*</label>
+                  <Field
+                    id="lastname"
+                    type="text"
+                    name="lastname"
+                    placeholder="Ваше прізвище"
+                    className={getInputClass(errors.lastname, touched.lastname)}
+                  />
+                  <ErrorMessage
+                    name="lastname"
+                    component="p"
+                    className={css.error}
+                  />
+                </div>
+              </div>
+              <div className={css.label}>
+                <label htmlFor="phone">Номер телефону*</label>
+                <Field
+                  id="phone"
+                  className={getInputClass(errors.phone, touched.phone)}
+                  type="tel"
+                  name="phone"
+                  placeholder="+38(0__) ___- __ - __"
+                />
+                <ErrorMessage
+                  name="phone"
+                  component="p"
+                  className={css.error}
+                />
+              </div>
+              <div className={css.label_wrapper}>
+                <div className={css.label}>
+                  <label htmlFor="city">Місто доставки*</label>
+                  <Field
+                    id="city"
+                    type="text"
+                    name="city"
+                    className={getInputClass(errors.city, touched.city)}
+                    placeholder="Ваше місто"
+                  />
+                  <ErrorMessage
+                    name="city"
+                    component="p"
+                    className={css.error}
+                  />
+                </div>
+                <div className={css.label}>
+                  <label htmlFor="warehoseNumber">
+                    Номер відділення Нової Пошти*
+                  </label>
 
-									<Field id="comment" className={css.input} type="text" name="comment" />
-									<ErrorMessage name="comment" component="p" className={css.error} />
-								</div>
-							)}
-						</fieldset>
-
-						{!isOrder ? (
-							<button className={css.button} type="submit" disabled={isSubmitting || mutation.isPending}>
-								{mutation.isPending ? "Збереження..." : "Зберегти зміни"}
-							</button>
-						) : (
-							<button className={css.button} type="submit" disabled={isSubmitting || mutation.isPending}>
-								{mutation.isPending ? "Оформлення замовлення..." : "Оформити замовлення"}
-							</button>
-						)}
-					</Form>
-				)}
-			</Formik>
-		</div>
-	)
+                  <Field
+                    id="warehoseNumber"
+                    type="text"
+                    name="warehoseNumber"
+                    placeholder="Ваш номер відділення"
+                    className={getInputClass(
+                      errors.warehoseNumber,
+                      touched.warehoseNumber
+                    )}
+                  />
+                  <ErrorMessage
+                    name="warehoseNumber"
+                    component="p"
+                    className={css.error}
+                  />
+                </div>
+              </div>
+            </fieldset>
+            <button
+              className={css.button}
+              type="submit"
+              disabled={isSubmitting || mutation.isPending}
+            >
+              {mutation.isPending ? 'Збереження...' : 'Зберегти зміни'}
+            </button>
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
 }
